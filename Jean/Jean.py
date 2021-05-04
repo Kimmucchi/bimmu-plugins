1a102998e2fb5fec68835e5c12120ebf1f679039
Kimmucchi <quennebelle@gmail.com>
2021-05-04 10:04:23 -0700

    blah

--- Jean/Jean.py
+++ Jean/Jean.py
@@ -1,4 +1,4 @@
-import rate_artifact_jean as raDPS
+import .artifact.rate_artifact_jean as raDPS
 #############################################################
 import datetime, time, urllib.parse, re, asyncio, json
 import discord
