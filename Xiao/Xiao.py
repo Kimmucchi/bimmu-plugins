e21a859bcdbabaedb63551d5e9c0861f64bd8974
Kimmucchi <quennebelle@gmail.com>
2021-06-12 18:57:03 -0700

    Update Xiao.py

--- Xiao/Xiao.py
+++ Xiao/Xiao.py
@@ -83,7 +83,7 @@ class Xiao(commands.Cog):
     # Convert to TW/HK/MO
     now_thm = now.astimezone(timezone('Etc/GMT-8'))
 
-    embed = discord.Embed(title="Server Status", colour=discord.Colour(0xcbffff), description=f'```md\n# NA {now_na.strftime("%I:%M %p")}\r\n```\
+    embed = discord.Embed(title="Server Status", colour=discord.Colour(0x1d515b), description=f'```md\n# NA {now_na.strftime("%I:%M %p")}\r\n```\
     • Daily reset in {hr_na} hours and {min_na} minutes!\r\n\
     • Weekly reset in {day_na} days, {hr_na} hours and {min_na} minutes\r\n\
     ```bash\n# EU {now_eu.strftime("%I:%M %p")}\r\n```\
